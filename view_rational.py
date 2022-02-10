7da58ad826315ffb3ee59a059f69ebec14b7e2d4
Vas-Yan <92777445+Vas-Yan@users.noreply.github.com>
2022-02-10 21:51:30 +0300

    Add files via upload
    added

---
+++ view_rational.py
@@ -0,0 +1,8 @@
+def view_data(data, title):
+    print(f'{title} = {data}')
+
+def get_value():
+    return float(input('value = '))
+
+def get_function():
+    return str(input('choose + , - , * , / = '))
